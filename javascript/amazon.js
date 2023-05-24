bfd0a35003f354b7420eaca6d82aaa1b2053ba86
Allen Labrague <[allenlabrague06@gmail.com]>
2023-05-24 09:16:44 +0800

    Add an added text

--- javascript/amazon.js
+++ javascript/amazon.js
@@ -78,11 +78,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       `.js-added-to-cart-${productId}`
     );
 
-    addedMessage.classList.add("added-to-cart-visible");
-
     setTimeout(() => {
       addedMessage.classList.remove("added-to-cart-visible");
     }, 2000);
+
+    addedMessage.classList.add("added-to-cart-visible");
 
     if (matchingItem) {
       matchingItem.quantity += quantity;
